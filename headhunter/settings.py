76562509e2f9b627f5a1fa8d249f44f80c293b7e
Mykhailo Ovcharenko <nuredev@github.com>
2023-05-30 22:25:49 +0300

    static as always

--- headhunter/settings.py
+++ headhunter/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -27,7 +26,8 @@ SECRET_KEY = "7a9a6935074ff91e4da0ab2344259b99127217c536583eee50861b61121bcc0cbd
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    "django-server-production-fac1.up.railway.app"
+    "django-server-production-fac1.up.railway.app",
+    "127.0.0.1"
 ]
 
 # OAuth
@@ -146,11 +146,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'frontend', 'static')
 STATIC_URL = "static/"
-STATICFILES_DIRS = [
-    BASE_DIR / "frontend" / "static",
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "frontend" / "static",
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
